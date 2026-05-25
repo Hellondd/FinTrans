161f29f4c2136928d72bf4ca2889adcc7af65539
Mar25119 <Zaradkaka@gmail.com>
2026-05-25 23:19:50 +0300

    Delete FinTrans-services-fraud_detection.py directory

--- FinTrans-services-fraud_detection.py/backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .client import Client
-from .contact import Contact
-from .address import Address
-from .document import Document
-from .risk_profile import RiskProfile
-from .product import Product
-from .transaction import Transaction
-from .user import User  # для авторизации
--- FinTrans-services-fraud_detection.py/backend/app/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-from .client import ClientRead, ClientFilter, ClientCreate, ClientUpdate
-from .auth import Token, TokenData, UserCreate, UserRead
-from .transaction import TransactionCreate, TransactionUpdate, TransactionRead, TransactionFilter
