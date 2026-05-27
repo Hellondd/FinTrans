c2b991eb7c0d80d78d7847c7508c2c7cefb5aba5
VlAtrosh <Vlasosva99@gmail.com>
2026-05-27 06:17:35 +0300

    feat: cors main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.api.v1.endpoints import auth, clients, transactions
-from app.api.v1.endpoints import dashboard
-from app.api.v1.endpoints import import_data
+from app.api.v1.endpoints import auth, clients, transactions, dashboard, import_data
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(auth.router, prefix=settings.API_V1_STR)
 app.include_router(clients.router, prefix=settings.API_V1_STR)
